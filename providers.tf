e5618affcecdfd2679faac1d164278a4f67e402a
Cody Gagnon <codycodes@users.noreply.github.com>
2025-05-25 09:01:32 -0700

    auto-register Microsoft.AzureTerraform provider

--- providers.tf
+++ providers.tf
@@ -1,13 +1,7 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   subscription_id                 = var.subscription_id
-  resource_provider_registrations = "none" # TODO: register resource provider
+  resource_provider_registrations = "none"
+  resource_providers_to_register  = ["Microsoft.AzureTerraform"]
   features {}
 }
-
-# TODO: register provider
-# TODO: remove dependence on az command
-# TODO: generate a providers.tf and main.tf
-# TODO: provide instructions on what to do after import - remove sensitive values if needed, etc.
-# TODO: only generate debug files if there's an error
-# TODO: for custom naming of resources generated?  # TODO: revisit this based on number of resources
